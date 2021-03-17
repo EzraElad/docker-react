31087ab830bf457a5381145633db45bf8ebe1d19
Elad Ezra <xxeladxx@gmail.com>
2021-03-17 15:54:18 +0200

    Changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ELADDD
+          I Was changed on the feature branch
         </p>
         <p>
           Elad Ezra
